7e9edb3a5fb890d3981a09f2acbfd08065102218
jama303 <johnmartinez.jm.jm@gmail.com>
2018-12-17 15:24:49 -0500

    Variables

---
+++ fundamentos.js
@@ -0,0 +1,6 @@
+var nombre = "sergio  "
+var apellido = "ariza "
+var nombreenmayusculas = nombre.toUpperCase()
+var apellidoenminusculas = apellido.toLocaleLowerCase()
+var primeraletradelnombre = nombre.charAt(0)
+var cantidaddeletrasdelnombre = nombre.length
