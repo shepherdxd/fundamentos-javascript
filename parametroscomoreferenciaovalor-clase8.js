4a01b2e2e64058ab3a39ed71ce7551ab0d639d99
shepherdxd <sergioandres4999@yahoo.es>
2018-12-18 15:07:46 -0500

    parametroscomoreferencia

--- parametroscomoreferenciaovalor-clase8.js
+++ parametroscomoreferenciaovalor-clase8.js
@@ -1,3 +1,43 @@
+var  persona = { sergio : {
+        nombre: "sergio",
+        apellido: "ariza",
+        edad: 20
+    },
+    sacha : {
+        nombre: "sacha",
+        apellido: "nicolaevic",
+        edad: 28
+    }
+}
+
+
+function imprimirNombreEnMayusculas(persona) {
+    //var   nombre = persona.nombre
+
+    console.log(persona.nombre.toUpperCase())
+
+
+}
+imprimirNombreEnMayusculas(sergio)
+// imprimirNombreEnMayusculas({nombre:"pepito"})
+
+var sergio = {
+    nombre: "sergio",
+    edad: 20
+}
+var jesus = {
+    nombre: "jesus",
+    edad: 17
+}
+
+function imprimirNombreyEdad(persona) {
+    var { nombre, edad } = persona
+    console.log("hola soy " + sergio.nombre.toUpperCase() + " y tengo  " + sergio.edad + " años")
+    console.log("hola soy " + jesus.nombre.toUpperCase() + " y tengo " + jesus.edad + "años")
+
+}
+imprimirNombreyEdad(nombre = "sergio", 20)
+imprimirNombreyEdad(nombre = "jesus", 17)
 
 
 
@@ -6,7 +46,7 @@ imprimirNombreEnMayusculas(sergio)
 imprimirNombreEnMayusculas(sacha)
 //imprimirNombreEnMayusculas({nombre:"pepito"})
 
-var personas = {sergio , jesus}
+var personas = { sergio, jesus }
 var sergio = {
     nombre: "sergio",
     edad: 20
